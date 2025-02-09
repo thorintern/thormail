b40fb7cca3f1c607fbd4e19262c1e18bafca8713
Thor Intern <intern@thorchain.net>
2025-02-09 16:31:07 +0400

    fix global

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['storage.googleapis.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        global: require.resolve('global'),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
