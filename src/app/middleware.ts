686dafa34212dfa1b1edf582d68d62a54e764344
Thor Intern <intern@thorchain.net>
2025-02-09 17:22:00 +0400

    ...

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,8 +1,13 @@
-import { NextResponse } from 'next/server';
-import crypto from 'node:crypto';
+import { NextResponse } from "next/server";
+import crypto from "node:crypto";
 
-if (typeof globalThis.crypto === 'undefined') {
+if (typeof globalThis.crypto === "undefined") {
   globalThis.crypto = crypto.webcrypto as Crypto;
+}
+
+if (typeof global === "undefined") {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).global = window;
 }
 
 export function middleware() {
