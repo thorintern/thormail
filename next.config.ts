931e267acb5f52dae1ab8970b86b8e219304dd2d
Thor Intern <intern@thorchain.net>
2025-02-09 16:35:23 +0400

    undo

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['storage.googleapis.com'],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        global: require.resolve('global'),
-      };
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
