5ce2e88f86daf5e6549b571a269889d56cd06e1e
Thor Intern <intern@thorchain.net>
2025-02-09 17:35:42 +0400

    refactor: Remove vm-browserify from Next.js config fallback

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
         ...config.resolve.fallback,
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
-        vm: require.resolve('vm-browserify')
       };
     }
     return config;
