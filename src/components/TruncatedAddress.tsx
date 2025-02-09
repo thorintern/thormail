62bbe0caaf2cd343777067471918c0e5e21032f5
Thor Intern <intern@thorchain.net>
2025-02-09 16:18:05 +0400

    fix build

--- src/components/TruncatedAddress.tsx
+++ src/components/TruncatedAddress.tsx
@@ -22,6 +22,7 @@ export function TruncatedAddress({ address }: TruncatedAddressProps) {
       setTimeout(() => setIsCopied(false), 2000);
     } catch (_error) {
       toast.error("Failed to copy address");
+      console.error(_error);
     }
   };
 
